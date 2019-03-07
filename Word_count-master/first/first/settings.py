a9b3233a686f660b9e6ee8fa6750437feff2683f
limjeongmin <jml8740@gmail.com>
2019-03-07 17:45:12 +0900

    blog

--- Word_count-master/first/first/settings.py
+++ Word_count-master/first/first/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'wordcount.apps.WordcountConfig',
+    'blog.apps.BlogConfig',
 ]
 
 MIDDLEWARE = [
--- Word_count-master/first/first/urls.py
+++ Word_count-master/first/first/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 import wordcount.views
 
 urlpatterns = [
@@ -22,4 +22,5 @@ urlpatterns = [
     path('', wordcount.views.index, name='index'),
     path('about/', wordcount.views.about, name='about'),
     path('result/', wordcount.views.result, name='result'),
+    path('blog/',include('blog.urls'),name='blog')
 ]
